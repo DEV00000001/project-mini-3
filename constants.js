2736bbffdf6ff20cc92e79a5899169cd62f7a647
DEV00000001 <vuongdachaivang@gmail.com>
2025-06-22 21:56:22 +0700

    hehe2

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const PATH = "http://localhost:3000";
+const PATH = import.meta.env.VITE_API_URL ;
 
 export const API_PRODUCT = `${PATH}/products`;
 export const API_CATEGORIES = `${PATH}/categories`;
